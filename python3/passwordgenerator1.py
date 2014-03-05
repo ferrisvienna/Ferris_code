fc8d1ad7b1133329f07d2be3b700361092e7ce28
Ferris B. <bartakferris@gmail.com>
2014-03-05 16:35:29 +0000

    Update passwordgenerator1.py
    added time delay.

--- python3/passwordgenerator1.py
+++ python3/passwordgenerator1.py
@@ -1,12 +1,21 @@
+#By ferrisofvienna
+
 import easygui as e
 import random as r
+import time as t
 
+
+alphabet="1","2"
 use=e.enterbox(msg="Enter the location (=application, website,..) where you want to use your password?")
+t.sleep (1/2)
 word=e.enterbox(msg="enter your password-word")
 number=r.randint(1000,10000)
+
 password=("{}{}".format(word,number))
 e.msgbox(msg="your generated password for {} is {}".format(use,password))
+t.sleep(1/2)
 f=open("passwords.txt","a") # append, dranfügen
 f.write("Use: {} Password: {} \n".format(use,password))
 f.close()    
+t.sleep(1/2)
 e.msgbox("please check your new updated file passwords.txt")
