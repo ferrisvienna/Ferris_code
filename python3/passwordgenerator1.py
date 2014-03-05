aea3975b366075d0735a1132e22d7c8b9de2db0f
Ferris B. <bartakferris@gmail.com>
2014-03-05 16:44:18 +0100

    Update passwordgenerator1.py
    included use of password.

--- python3/passwordgenerator1.py
+++ python3/passwordgenerator1.py
@@ -1,12 +1,12 @@
 import easygui as e
 import random as r
 
-
+use=e.enterbox(msg="Enter the location (=application, website,..) where you want to use your password?")
 word=e.enterbox(msg="enter your password-word")
 number=r.randint(1000,10000)
 password=("{}{}".format(word,number))
-e.msgbox(msg="your generated password is {}".format(password))
+e.msgbox(msg="your generated password for {} is {}".format(use,password))
 f=open("passwords.txt","a") # append, dranfügen
-f.write("Password: {} \n".format(password))
+f.write("Use: {} Password: {} \n".format(use,password))
 f.close()    
 e.msgbox("please check your new updated file passwords.txt")
